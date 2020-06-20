a43d504e1ef992ec2cee1005d65f629e07ba4902
Luke Carr <me@lukecarr.dev>
2020-06-20 21:10:33 +0100

    build(gulp): added terser for code mangling/compression
    Signed-off-by: Luke Carr <me@lukecarr.dev>

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const { series, src, dest } = require("gulp");
 const del = require("del");
 const babel = require("gulp-babel");
+const terser = require("gulp-terser-js");
 
 const clean = () => del("dist/");
 
-const build = () => src("src/**/*.ts").pipe(babel()).pipe(dest("dist/"));
+const build = () => src("src/**/*.ts")
+  .pipe(babel())
+  .pipe(terser())
+  .pipe(dest("dist/"));
 
 exports.clean = clean;
 exports.build = series(clean, build);
